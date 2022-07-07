aa60e7fb1d1f1784c7a2f4a86809082ed2dc67ab
udaymattam <105538517+udaymattam@users.noreply.github.com>
2022-07-07 15:31:18 +0530

    Add files via upload

---
+++ Callback functions/callback.js
@@ -0,0 +1,9 @@
+function myDisplayer(some){
+    document.getElementById("heloo").innerHTML=some;
+}
+function myCalculator(num1,num2){
+    let sum=num1+num2;
+    return sum;
+}
+let result = myCalculator(5,5);
+myDisplayer(result);
